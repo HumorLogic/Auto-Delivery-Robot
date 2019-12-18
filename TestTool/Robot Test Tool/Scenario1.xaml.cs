6354484689c46f566ff020f0aa5a61849d516861
Humor <humor.ylw@outlook.com>
2019-12-18 23:03:15 +0800

    add device dynamic wather

--- TestTool/Robot Test Tool/Scenario1.xaml.cs
+++ TestTool/Robot Test Tool/Scenario1.xaml.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿# region Include
+using System;
 using System.Collections.Generic;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+
 using Windows.Devices.Enumeration;
 using Windows.Devices.SerialCommunication;
+using Windows.Foundation;
+
 using System.Collections.ObjectModel;
 using Windows.Storage.Streams;
 using System.Threading;
 using System.Threading.Tasks;
+using Robot_Test_Tool.Model;
+using System.Diagnostics;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+#endregion
 
 namespace Robot_Test_Tool
 {
@@ -18,30 +26,51 @@ namespace Robot_Test_Tool
     /// </summary>
     public sealed partial class Scenario1 : Page
     {
+        #region Members
 
         private SerialDevice serialPort = null;
+        private string aqs = null;
+        private DeviceWatcher deviceWatcher;
         DataWriter dataWriteObject = null;
+
+        private Dictionary<DeviceWatcher, String> mapDeviceWatchersToDeviceSelector;
+        private Boolean watcherStarted;
+        private Boolean isAllDevicesEnumerated;
+
         private ObservableCollection<DeviceInformation> listOfDevice;
         private List<string> AllPortName = new List<string>();
         private Dictionary<string, object> serialPortDeviceDic = new Dictionary<string, object>();
-
+        private string message;
+        public List<SerialPortSettingsModel> BaudRate { get; private set; }
         UInt32[] baudRate = { 9600, 19200, 38400, 57600, 115200 };
 
-        private string message;
+        #endregion
+
+        #region Constructor
         public Scenario1()
         {
             this.InitializeComponent();
+
+            mapDeviceWatchersToDeviceSelector = new Dictionary<DeviceWatcher, string>();
+            watcherStarted = false;
+            isAllDevicesEnumerated = false;
+
+
             listOfDevice = new ObservableCollection<DeviceInformation>();
             ListAvailablePorts();
             InitComboxItem();
+            InitializeDeviceWatcher();
+            StartDeviceWatchers();
         }
 
+        #endregion
 
+        #region Methods
         private async void ListAvailablePorts()
         {
             try
             {
-                string aqs = SerialDevice.GetDeviceSelector();
+                aqs = SerialDevice.GetDeviceSelector();
                 var dis = await DeviceInformation.FindAllAsync(aqs);
 
                 for (int i = 0; i < dis.Count; i++)
@@ -59,6 +88,122 @@ namespace Robot_Test_Tool
             {
                 Console.WriteLine("List AvailablePorts Failed");
                 Console.WriteLine(ex);
+            }
+        }
+
+        /// <summary>
+        /// Close the serial port device关闭串口设备
+        /// </summary>
+        private void CloseDevice()
+        {
+            if (serialPort != null)
+            {
+                serialPort.Dispose();
+            }
+            serialPort = null;
+            listOfDevice.Clear();
+            AllPortName.Clear();
+            serialPortDeviceDic.Clear();
+        }
+
+
+        private async Task WriteAsync()
+        {
+            //throw new NotImplementedException();
+            Task<UInt32> storeAsyncTask;
+
+            dataWriteObject.WriteString(message);
+            storeAsyncTask = dataWriteObject.StoreAsync().AsTask();
+            UInt32 bytesWritten = await storeAsyncTask;
+        }
+
+        /// <summary>
+        /// 初始化DeviceWatcher
+        /// </summary>
+        private void InitializeDeviceWatcher()
+        {
+            // var deviceSelector = SerialDevice.GetDeviceSelector();
+            deviceWatcher = DeviceInformation.CreateWatcher(aqs);
+            AddDeviceWatcher(deviceWatcher, aqs);
+        }
+
+        private void AddDeviceWatcher(DeviceWatcher deviceWatcher, String deviceSelector)
+        {
+            deviceWatcher.Added += new TypedEventHandler<DeviceWatcher, DeviceInformation>(this.OnDeviceAdded);
+            deviceWatcher.Removed += new TypedEventHandler<DeviceWatcher, DeviceInformationUpdate>(this.OnDeviceRemoved);
+            deviceWatcher.EnumerationCompleted += new TypedEventHandler<DeviceWatcher, Object>(this.OnDeviceEnumerationComplete);
+
+            // mapDeviceWatchersToDeviceSelector.Add(deviceWatcher, deviceSelector);
+        }
+
+        private void OnDeviceAdded(DeviceWatcher sender, DeviceInformation args)
+        {
+            //TipText.Text = args.Name + "加入";
+            Debug.WriteLine(args.Name + " Serial Port Added");
+            //try
+            //{
+            //    TipText.Text = args.Name + "加入";
+            //}
+            //catch (Exception ex)
+            //{
+            //    Debug.WriteLine(ex.Message);
+            //}
+
+
+        }
+
+        private void OnDeviceEnumerationComplete(DeviceWatcher sender, object args)
+        {
+            
+            Debug.WriteLine("Device Enumeration Completed");
+        }
+
+        private void OnDeviceRemoved(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            Debug.WriteLine("Device Removed");
+        }
+
+
+        /// <summary>
+        /// 打开Device Watcher
+        /// </summary>
+        private void StartDeviceWatchers()
+        {
+            //Start all device watcher
+            watcherStarted = true;
+            isAllDevicesEnumerated = false;
+            deviceWatcher.Start();
+
+            //foreach (DeviceWatcher deviceWatcher in mapDeviceWatchersToDeviceSelector.Keys)
+            //{
+            //    if((deviceWatcher.Status!=DeviceWatcherStatus.Started)
+            //        && (deviceWatcher.Status != DeviceWatcherStatus.EnumerationCompleted))
+            //    {
+            //        deviceWatcher.Start();
+            //    }
+
+            //}
+        }
+
+
+        #region UI Event Method
+
+
+        /// <summary>
+        /// 初始化波特率下拉的选择内容
+        /// </summary>
+        private void InitComboxItem()
+        {
+
+            //foreach (int i in baudRate)
+            //{
+            //    BaudRateComboBox.Items.Add(i);
+            //}
+            //BaudRateComboBox.Items.Add(SerialPortSettingsModel.Instance.getBaudRates());
+            BaudRate = SerialPortSettingsModel.Instance.getBaudRates();
+            for (int i = 0; i < BaudRate.Count; i++)
+            {
+                BaudRateComboBox.Items.Add(BaudRate[i].BaudRateValue);
             }
         }
 
@@ -93,7 +238,7 @@ namespace Robot_Test_Tool
                 // Configure serial settings
                 serialPort.WriteTimeout = TimeSpan.FromMilliseconds(1000);
                 serialPort.ReadTimeout = TimeSpan.FromMilliseconds(1000);
-                serialPort.BaudRate = baudRate[BaudRateComboBox.SelectedIndex];
+                serialPort.BaudRate = BaudRate[BaudRateComboBox.SelectedIndex].BaudRateValue;
                 //serialPort.BaudRate = (uint)BaudRateComboBox.SelectedValue;
                 serialPort.Parity = SerialParity.None;
                 serialPort.StopBits = SerialStopBitCount.One;
@@ -108,18 +253,6 @@ namespace Robot_Test_Tool
             }
 
         }
-
-        /// <summary>
-        /// 初始化波特率下拉的选择内容
-        /// </summary>
-        private void InitComboxItem()
-        {
-            foreach (int i in baudRate)
-            {
-                BaudRateComboBox.Items.Add(i);
-            }
-        }
-
 
         private void PortNameComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -150,9 +283,9 @@ namespace Robot_Test_Tool
             //        }
             //    }
             //}
+            //ListAvailablePorts();
+
         }
-
-
 
         private async void SendMsg_Click(object sender, RoutedEventArgs e)
         {
@@ -173,16 +306,6 @@ namespace Robot_Test_Tool
                 //Launch the WriteAsync task to perform the write
                 await WriteAsync();
             }
-        }
-
-        private async Task WriteAsync()
-        {
-            //throw new NotImplementedException();
-            Task<UInt32> storeAsyncTask;
-
-            dataWriteObject.WriteString(message);
-            storeAsyncTask = dataWriteObject.StoreAsync().AsTask();
-            UInt32 bytesWritten = await storeAsyncTask;
         }
 
         private void BaudRateComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -233,8 +356,12 @@ namespace Robot_Test_Tool
                 await WriteAsync();
             }
         }
+
+        #endregion
+
+
+        #endregion
+
     }
-
-
 }
 
